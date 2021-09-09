e61648904ff15338683c826822f44d3f90c2d9a0
Roberto Villafa <metheoro4775@gmail.com>
2021-09-09 19:55:31 -0300

    primero

--- models/server.js
+++ models/server.js
@@ -1,21 +1,69 @@
-const express = require("express");
+const express = require('express')
+const cors = require('express')
+//importo conex a DB
+const {dbConnection} = require('../database/config')
 
 class Server {
-	constructor() {
-		this.app = express();
 
-		this.middlewares();
-	}
+    constructor(){
+    //defino lo que quiero que se inicialice cuando levanate el server
 
-	middlewares() {
-		this.app.use(express.static("public"));
-	}
+    this.app = express()
+    this.usuariosPath="/api/usuarios"
+    this.pizzasPath="/api/pizzas"
+    this.authPath="/api/auth"
 
-	listen() {
-		this.app.listen(process.env.PORT, () => {
-			console.log("Server on port", process.env.PORT);
-		});
-	}
+    //comnexion
+    this.conectarDB();
+
+    //middlewares
+    this.middlewares();
+
+    //rutas
+    this.routes();
+
+
+    }
+
+    //funcion para conectar la DB
+
+    async conectarDB(){
+        await dbConnection()
+    }
+
+    middlewares(){
+
+       //Carpeta publica
+       
+       this.app.use(express.static("public"));
+
+       //Cors
+       this.app.use(cors());
+
+       //Acceso al Body
+       this.app.use(express.json());
+       this.app.use(express.urlencoded({extended:true}));
+       
+
+
+
+
+
+    }
+
+    routes(){
+        this.app.use(this.authPath, require("../routes/auth"))
+        this.app.use(this.usuariosPath, require("../routes/usuarios"))
+        this.app.use(this.pizzasPath, require("../routes/pizzas"))
+    }
+
+    listen(){
+        this.app.listen(process.env.PORT, ()=>{
+            console.log("Server onLine");
+        });
+        
+    }
+
 }
 
-module.exports = Server;
+module.exports = Server
